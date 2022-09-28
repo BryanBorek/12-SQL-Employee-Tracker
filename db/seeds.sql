c18361dfcb613d5b38120e3d64532c5e3da4e6fa
Bryan Borek <bryantborek@gmail.com>
2022-09-28 18:23:45 -0500

    building inquirerprompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,18 @@
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (1, 'Bry', 'B', 1, null),
-        (2, 'Ely', 'Clown', 2, null);
-
-INSERT INTO employee_role (id, title, salary, department_id)
-VALUES  (1, 'Salesperson', 200000, 1),
-        (2, 'Lead Engineer', 111000, 2),
-        (2, 'Software Engineer', 99000, 2),
-        (3, 'Account Manager', 10257500, 3),
-        (4, 'Accountant', 8000, 3),
-        (5, 'Legal Team Lead', 69420, 4),
-        (6, 'Lawyer', 399, 4);
-
 INSERT INTO department (id, department_name)
 VALUES  (1, 'Sales'),
         (2, 'Engineering'),
         (3, 'Finance'),
         (4, 'Legal');
+        
+INSERT INTO employee_role (id, title, salary, department_id)
+VALUES  (1, 'Salesperson', 200000.00, 1),
+        (2, 'Lead Engineer', 111000.00, 2),
+        (3, 'Software Engineer', 99000.00, 2),
+        (4, 'Account Manager', 10257500.00, 3),
+        (5, 'Accountant', 8000.00, 3),
+        (6, 'Legal Team Lead', 69420.00, 4),
+        (7, 'Lawyer', 399.00, 4);
 
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (1, 'Bry', 'Borek', 1, null),
+        (2, 'Apples', 'The Horse', 2, 1);
--- server.js
+++ server.js
@@ -1,45 +1,219 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const cTable = require('console.table');
 
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    user: 'root',
+    password: '',
+    database: 'employees_db'
   },
-  console.log(`Connected to the employee_db database.`)
+  console.log(`Connected to the employees_db database.`)
 );
 
-app.get('api/movies', (req, res) => {
-    db.query('SELECT * FROM employee', (err, data) => err ? res.status(500).json(err) : res.json(data))
-})
-app.post('/api/add-employee', (req, res) => {
-    req.body
-    db.query('INSERT INTO employee () employee')
-})
+function accessDb() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update employee role', 'Exit'],
+      message: 'What would you like to do now?',
+      name: 'choices'
+    }
+  ])
+    .then((answers) => {
+      switch (answers.choices) {
+        case 'View all departments':
+          db.query('SELECT * FROM department', (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log('Please see departments below')
+            console.log(results);
 
+            console.table(results);
+            accessDb()
+          });
+          break;
 
+        case 'View all roles':
+          db.query('SELECT employee_role.title , employee_role.salary , department.department_name FROM employee_role , department WHERE employee_role.department_id = department.id;', (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log('Please see roles below')
+            console.table(results);
+            accessDb()
+          });
+          break;
 
+        case 'View all employees':
+          db.query('SELECT B.first_name , B.last_name, A.first_name AS manager , employee_role.title , employee_role.salary , department.department_name FROM employee A, employee B, employee_role , department WHERE A.id = B.manager_id AND B.role_id = employee_role.id AND employee_role.department_id = department.id;', (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log('Please see employees below')
+            console.table(results);
+            accessDb()
+          });
+          break;
 
+        case 'Add a department':
+          addDpt()
+          break;
 
+        case 'Add a role':
+          addRole()
+          break;
 
+        case 'Add an employee':
+          addEmp()
+          break;
 
+        case 'Update employee role':
+          updateEmp()
+          break;
 
-app.get('/api/movies', (req, res) => res.json(db));
+        case 'Exit':
+          console.log('Goodbye')
+          break;
 
-app.post('/api/movies', (req, res) => {
-    db.query
-})
+        default:
+          console.log('Goodbye')
+          break;
+      }
+    })
+}
+accessDb();
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+function addDpt() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the new department name?',
+      name: 'newDpt'
+    }
+  ])
+    .then((answers) => {
+      db.query(`INSERT INTO department (department_name)
+    VALUES  ('${answers.newDpt}');`, (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      db.query('SELECT * FROM department', (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('Please see departments below')
+        console.table(results);
+        accessDb()
+      });
+    }
+    )
+}
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+function addRole() {
+  // let resultsToChoices;
+  // db.query('SELECT department.department_name FROM department', (err, results) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
+  //    resultsToChoices = JSON.stringify(results).replaceAll(`{"department_name":`,'').replaceAll('}','').replaceAll('[','').replaceAll(']','');
+  //   console.log(resultsToChoices);
+  // });
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the new role title?',
+      name: 'newRoleTitle'
+    },
+    {
+      type: 'input',
+      message: 'What is the new role salary?',
+      name: 'newRoleSalary'
+    },
+    {
+      type: 'list',
+      choices: ["Sales","Engineering","Finance","Legal"],
+      message: 'What department?',
+      name: 'newRoleDpt'
+    }
+  ])
+    .then((answers) => {
+      db.query(`INSERT INTO employee_role (title, salary, department_id)
+    VALUES  ('${answers.newRoleTitle}', '${answers.newRoleSalary}', '${answers.newRoleDpt}');`, (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      db.query('SELECT employee_role.title , employee_role.salary , department.department_name FROM employee_role , department WHERE employee_role.department_id = department.id;', (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('Please see roles below')
+        console.table(results);
+        accessDb()
+      });
+    }
+    )
+}
+
+function addEmp() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: `What is the new employee's first name?`,
+      name: 'newEmpFirst'
+    },
+    {
+      type: 'input',
+      message: `What is the new employee's last name?`,
+      name: 'newEmpLast'
+    },
+    {
+      type: 'list',
+      choices: ['Salesperson', 'Lead Engineer', 'Software Engineer', 'Account Manager', 'Accountant', 'Legal Team Lead', 'Lawyer'],
+      message: `What is the new employee's role?`,
+      name: 'newEmpRole'
+    },
+    {
+      type: 'list',
+      choices: ['Bry', 'Ely'],
+      message: `Who is the new employee's manager?`,
+      name: 'newEmpManager'
+    }
+  ])
+    .then((answers) => {
+      const first = answers.newEmpFirst;
+      const last = answers.newEmpLast;
+      let manager = 0;
+      switch (answers.newEmpManager) {
+        case 'Bry':
+          manager = 1;
+          break;
+        case 'Ely':
+          manager = 2;
+          break;
+      }
+      switch (answers.newEmpRole) {
+        case 'Salesperson':
+          db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES  ('${first}', '${last}', 1, '${manager}')`, (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+          db.query('SELECT B.first_name , B.last_name, A.first_name AS manager , employee_role.title , employee_role.salary , department.department_name FROM employee A, employee B, employee_role , department WHERE A.id = B.manager_id AND B.role_id = employee_role.id AND employee_role.department_id = department.id;', (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            console.log('Please see employees below')
+            console.table(results);
+            accessDb()
+          });
+          break;
+      }
+    })
+}
